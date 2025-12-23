80f3a01edb8ec53eebda417a269cd902fa746231
ravi-asati <129985277+ravi-asati@users.noreply.github.com>
2025-12-23 14:00:20 +0000

    Update cobol_to_parquet.py

--- articles/2025-12-mainframe-for-data-engineers/cobol-to-parquet/cobol_to_parquet.py
+++ articles/2025-12-mainframe-for-data-engineers/cobol-to-parquet/cobol_to_parquet.py
@@ -1,15 +1,14 @@
 import argparse
-from pyspark.sql import SparkSessio
+from pyspark.sql import SparkSession
 
 
 def main():
     p = argparse.ArgumentParser(description="Read COBOL (Cobrix) and write Parquet")
     p.add_argument("--input", required=True, help="Input COBOL file path (file:/... or /...)")
     p.add_argument("--copybook", required=True, help="Copybook .cpy path (local path)")
-    p.add_argument("--output", required=True, help="Output Parquet path (file:/... or /...)")
     p.add_argument("--record-length", default="24", help="Fixed record length in bytes (default 24)")
-    p.add_argument("--encoding", default="EBCDIC", help="Text encoding for PIC X fields (e.g., EBCDIC/cp037)")
-    p.add_argument("--print-charset", action="store_true", help="Print JVM charset resolution and exit?")
+    p.add_argument("--output", required=True, help="Output Parquet path (file:/... or /...)") 
+    p.add_argument("--encoding", default="EBCDIC", help="Text encoding for PIC X fields (default cp037)")
     args = p.parse_args()
 
     spark = (
@@ -18,20 +17,17 @@ def main():
         .getOrCreate()
     )
 
-    if args.print_charset:
-        spark.stop()
-        return
-
     # Cobrix reader
     df = (
         spark.read.format("cobol")
         .option("copybook", args.copybook)
-        .option("record_length", str(args.record_length))
+        .option("record_length", args.record_length)
         .option("encoding", args.encoding)
         .load(args.input)
     )
 
     df.printSchema()
+    #df.show(10, truncate=False)
 
     # Write Parquet
     (
@@ -43,16 +39,16 @@ def main():
     print(f"Wrote Parquet to: {args.output}")
     spark.stop()
 
-
 if __name__ == "__main__":
     main()
 
 '''
+# Provide all paths as absolute paths here
 spark-submit \
   --packages za.co.absa.cobrix:spark-cobol_2.13:2.9.4 \
-  convert_cobol_to_parquet_v2.py \
-  --input "file:./TRXN_COBOL_DATA.ebcdic" \
-  --copybook "./trxn_writer_simple.cpy" \
-  --output "file:./trxn_parquet" \
-  --record-length 24
+  cobol_to_parquet.py \
+  --input "file:/Users/aarvi/Ravi/DataEngineering/article/TRXN_COBOL_DATA.ebcdic" \
+  --copybook "/Users/aarvi/Ravi/DataEngineering/article/trxn_writer_simple.cpy" \
+  --record-length 24 \
+  --output "file:/Users/aarvi/Ravi/DataEngineering/article/trxn_parquet"
 '''
